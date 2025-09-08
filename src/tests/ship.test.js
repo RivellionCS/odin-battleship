846cbc833f2e7511bcd2a67ad9a9e8b9a525bc01
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-08 20:31:47 -0300

    refactor the test string for hit method

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -6,7 +6,7 @@ test("Create a new ship object and check if it is a ship", () => {
   expect(destroyer).toBeInstanceOf(Ship);
 });
 
-test("Check if the hit function works so that hit increments by 1", () => {
+test("Check if the hit function works so that hit increments from 0 to 1", () => {
   destroyer.hit();
   expect(destroyer.hits).toBe(1);
 });
