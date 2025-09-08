17e427752ede23a8df48e62c9c0b5ebccf690e6b
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-08 20:24:36 -0300

    setup jest to work with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-
+import eslintPluginJest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: { jest: eslintPluginJest },
+    languageOptions: { globals: eslintPluginJest.environments.globals.globals },
+    rules: { ...eslintPluginJest.configs.recommended.rules },
+  },
 ]);
