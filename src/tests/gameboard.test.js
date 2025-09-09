c8976592604191e6e77e88fa93bfaea52811b8a1
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-08 22:11:51 -0300

    create test to check board size

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -5,3 +5,10 @@ const board = new Gameboard();
 test("check if the created board object is of gameboard class", () => {
   expect(board).toBeInstanceOf(Gameboard);
 });
+
+const boardSize =
+  board.board.every((item) => item.length === 10) && board.board.length === 10;
+
+test("check if the created board class is an array of 10x10", () => {
+  expect(boardSize).toBeTruthy();
+});
