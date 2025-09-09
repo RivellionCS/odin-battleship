f5f30d49580fae98f23c40d33b0deac669a49ac4
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-08 21:08:46 -0300

    create gameboard test to check class

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,1 +1,7 @@
 import { Gameboard } from "../components/gameboard";
+
+const board = new Gameboard();
+
+test("check if the created board object is of gameboard class", () => {
+  expect(board).toBeInstanceOf(Gameboard);
+});
