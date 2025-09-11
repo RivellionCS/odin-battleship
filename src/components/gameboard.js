841a5a959d398034583e070dc64ea27803ae854b
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-11 20:10:00 -0300

    change gameboard array to contain an object

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -3,7 +3,7 @@ export { Gameboard };
 class Gameboard {
   constructor() {
     this.board = Array.from({ length: 10 }, () =>
-      Array.from({ length: 10 }, () => null)
+      Array.from({ length: 10 }, () => ({ shipObject: null, isHit: false }))
     );
   }
 }
