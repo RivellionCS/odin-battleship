3eddbb7b48f133f304dcbe97b97d54fc66076a61
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-14 03:57:43 -0300

    fix gameboard size test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -13,10 +13,10 @@ test("check if the created board object is of gameboard class", () => {
   expect(board).toBeInstanceOf(Gameboard);
 });
 
-const boardSize =
-  board.board.every((item) => item.length === 10) && board.board.length === 10;
-
 test("check if the created board class is an array of 10x10", () => {
+  const boardSize =
+    board.board.every((item) => item.length === 10) &&
+    board.board.length === 10;
   expect(boardSize).toBeTruthy();
 });
 
