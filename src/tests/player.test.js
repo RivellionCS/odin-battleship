9601550df3e13f4e55bbdf733fdfbc3c53f8a734
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-18 20:40:17 -0300

    create second test to check name

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -4,3 +4,8 @@ test("Expect a new player to be of Player class", () => {
   const testPlayer = new Player("John");
   expect(testPlayer).toBeInstanceOf(Player);
 });
+
+test("Expect the player class to store the player name", () => {
+  const testPlayer = new Player("Mario");
+  expect(testPlayer.name).toBe("Mario");
+});
