2dcd758529c4f718bc8f3e42016f4895ccb076a8
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-18 20:47:43 -0300

    add test to check if player type works

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -9,3 +9,10 @@ test("Expect the player class to store the player name", () => {
   const testPlayer = new Player("Mario");
   expect(testPlayer.name).toBe("Mario");
 });
+
+test("Expect a human player to be human and a computer player to be computer", () => {
+  const testHuman = new Player("Ted", "human");
+  const testComputer = new Player("AM", "computer");
+  expect(testHuman.type).toBe("human");
+  expect(testComputer.type).toBe("computer");
+});
