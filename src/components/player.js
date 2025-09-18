8622edbf5b5ab922d2bf147e6d0d870306841ac8
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-18 20:53:17 -0300

    make players have their own gameboard

--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,11 @@
+import { Gameboard } from "./gameboard";
+
 export { Player };
 
 class Player {
   constructor(name, type) {
     this.name = name;
     this.type = type;
+    this.gameboard = new Gameboard();
   }
 }
