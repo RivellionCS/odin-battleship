baa05638113a0f9358698a48964b65863be7938b
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-09-20 04:13:51 -0300

    render gameboards using js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./styles/main.css";
 
 console.log("hello world");
+
+function renderGameboards() {
+  const playerBoard = document.getElementById("board1");
+  const computerBoard = document.getElementById("board2");
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const playerButton = document.createElement("button");
+      const computerButton = document.createElement("button");
+      playerButton.dataset.playerPosition = `${i}${j}`;
+      computerButton.dataset.computerPosition = `${i}${j}`;
+      playerBoard.appendChild(playerButton);
+      computerBoard.appendChild(computerButton);
+    }
+  }
+}
+
+renderGameboards();
